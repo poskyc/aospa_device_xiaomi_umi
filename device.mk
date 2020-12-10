6580691b0b65e914ef2b6c3e18b3daf64034fbf0
Vishalcj17 <vishalcj@aospa.co>
2020-12-10 19:54:54 +0530

    umi: audio: Fixup A2DP when connected to an audio device.
    Causes SystemUI to crash, so remove the A2DP entries altogether and just include the A2DP config copied from frameworks/av.
    
    Change-Id: I7ec913f5bca19256918d03c19cdb12408c9b3954

--- device.mk
+++ device.mk
@@ -29,6 +29,7 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
+    frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
 
 # Bluetooth
