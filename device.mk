d4cd2a9060c78ff908578f899f1718ba60740cf9
Alexander Koskovich <zvnexus@outlook.com>
2021-03-20 19:08:05 -0700

    umi: Import vendor WiFi & framework overlays.
     * From LA.UM.9.12.r1-12500-SMxx50.0
    
    Change-Id: I3d426d23fe8aa5d4c1c535b772fc426125fae715

--- device.mk
+++ device.mk
@@ -113,6 +113,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Overlays
+PRODUCT_PACKAGES += \
+    FrameworksResTarget \
+    WifiResTarget
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
