591aa406a3321a7a711750716fe6999353d58e75
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-06-23 19:53:18 +0530

    umi: Import minimal device specific vendor properties
    Change-Id: Id7b1a5c218e4e7e67089c8ed4007867ec9b67081

--- device.mk
+++ device.mk
@@ -19,6 +19,10 @@ PRODUCT_PACKAGES += \
 # APEX
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Adreno
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.gfx.driver.1=com.qualcomm.qti.gpudrivers.kona.api30
+
 # Atrace
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
@@ -28,6 +32,33 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
 # Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+    aaudio.mmap_policy=1 \
+    persist.vendor.audio.delta.refresh=true \
+    persist.vendor.audio.misound.disable=true \
+    persist.vendor.audio.ring.filter.mask=0 \
+    ro.audio.monitorRotation=true \
+    ro.config.vc_call_vol_steps=11 \
+    ro.vendor.audio.enhance.support=false \
+    ro.vendor.audio.gain.support=true \
+    ro.vendor.audio.karaok.support=true \
+    ro.vendor.audio.ns.support=false \
+    ro.vendor.audio.scenario.support=true \
+    ro.vendor.audio.sdk.fluencetype=fluence \
+    ro.vendor.audio.soundfx.type=mi \
+    ro.vendor.audio.soundfx.usb=true \
+    ro.vendor.audio.us.proximity=true \
+    ro.vendor.audio.us.type=mius \
+    ro.vendor.audio.zoom.support=true \
+    ro.vendor.audio.zoom.type=1 \
+    vendor.audio.adm.buffering.ms=6 \
+    vendor.audio.feature.dynamic_ecns.enable=false \
+    vendor.audio.feature.spkr_prot.enable=false \
+    vendor.audio.hal.output.suspend.supported=false \
+    vendor.audio.offload.track.enable=false \
+    vendor.audio.spkcal.copy.inhal=true \
+    vendor.audio.usb.disable.sidetone=true
+
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
@@ -58,6 +89,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.qcom.bluetooth.soc=hastings
 
 # Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    camera.disable_zsl_mode=true
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
@@ -75,6 +109,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.charger.enable_suspend=1
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.cp.fcc_main_ua=400000 \
+    persist.vendor.cp.taper_term_mv=7000 \
+    persist.vendor.cp.qc3p5_vfloat_offset_uv=110000 \
+    persist.vendor.pps.disallowed=1 \
+    persist.vendor.hvdcp_opti.disallowed=1
+
 # Config Store
 PRODUCT_PACKAGES += \
     disable_configstore
@@ -87,12 +128,33 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
+# DPM
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.dpmhalservice.enable=1
+
 # DRM
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true
+
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3-service.clearkey
 
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
+# Data
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    persist.vendor.data.mode=concurrent \
+    ro.vendor.use_data_netmgrd=true
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.sf.color_mode=0 \
+    ro.vendor.display.sensortype=2 \
+    vendor.display.enable_async_powermode=0 \
+    vendor.display.qdcm.mode_combine=1 \
+    vendor.display.use_layer_ext=0 \
+    vendor.display.use_smooth_motion=0
 
 # Doze
 PRODUCT_PACKAGES += \
@@ -130,9 +192,34 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.strongbox_keystore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.strongbox_keystore.xml
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.crypto.allow_encrypt_override=true \
+    ro.crypto.volume.filenames_mode="aes-256-cts" \
+    ro.hardware.keystore_desede=true
+
+# Media (VPP)
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.media.vpp.enable=false \
+    debug.media.video.frc=false \
+    debug.media.video.vpp=false \
+    ro.vendor.media.video.frc.support=true \
+    ro.vendor.media.video.vpp.support=true \
+    vendor.media.vpp.aie.cade=100 \
+    vendor.media.vpp.aie.ltm=1 \
+    vendor.media.vpp.aie.ltmacebrih=0 \
+    vendor.media.vpp.aie.ltmacebril=20 \
+    vendor.media.vpp.aie.ltmacestr=37 \
+    vendor.media.vpp.aie.ltmsatgain=55 \
+    vendor.media.vpp.aie.ltmsatoff=55 \
+    vendor.media.vpp.debug.value.use=false
+
 # Namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+
+# Netflix
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.netflix.bsp_rev=Q8250-19134-1
 
 # Overlays
 PRODUCT_PACKAGES += \
