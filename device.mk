911124cdc172070eb188f6accd6cde128a53c4c4
chaptsand <chaptsand@gmail.com>
2021-08-18 13:38:57 +0800

    umi: Initial bringup
     * fstab was taken from LA.UM.9.12.r1-12500-SMxx50.0
    
    Change-Id: I3218f7ad746a0dad80e890ba5eb65444ba7b63c1

---
+++ device.mk
@@ -0,0 +1,54 @@
+# Copyright (C) 2021 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# APEX
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Charging
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.charger.enable_suspend=1
+
+# Config Store
+PRODUCT_PACKAGES += \
+    disable_configstore
+
+# Dalvik
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
+# Fastboot
+PRODUCT_PACKAGES += \
+    fastbootd
+
+# Init
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
+
+PRODUCT_PACKAGES += \
+    fstab.qcom
+
+# Namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Platform
+TARGET_BOARD_PLATFORM := kona
+
+# QTI
+TARGET_COMMON_QTI_COMPONENTS := all
+
+# Shipping API
+PRODUCT_SHIPPING_API_LEVEL := 29
