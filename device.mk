04cfde57fa395f442362fa358a66b82eeb83143f
chaptsand <chaptsand@gmail.com>
2021-08-19 01:20:16 +0800

    umi: Import vendor init scripts from LA.UM.9.12.r1-12500-SMxx50.0
    Change-Id: I96484524b48700dda9e8c81dd2e65f3f1938d0d0

--- device.mk
+++ device.mk
@@ -35,7 +35,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
 
 PRODUCT_PACKAGES += \
-    fstab.qcom
+    fstab.qcom \
+    init.qti.dcvs.sh \
+    init.target.rc
 
 # Namespaces
 PRODUCT_SOONG_NAMESPACES += \
