01859680bfd5854258b52a114b0994221fa29f85
chaptsand <chaptsand@gmail.com>
2021-08-19 19:09:13 +0800

    umi: Build atrace service
    Change-Id: I4afcebc5df597d24ee0d9863579925252a924113

--- device.mk
+++ device.mk
@@ -19,6 +19,10 @@ PRODUCT_PACKAGES += \
 # APEX
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Atrace
+PRODUCT_PACKAGES += \
+    android.hardware.atrace@1.0-service
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
