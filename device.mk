fe5608c416a93442b8c3325d3cd241941f9956be
chaptsand <chaptsand@gmail.com>
2021-08-19 22:52:30 +0800

    umi: Import audio_policy_configuration.xml from LA.UM.9.12.r1-12500-SMxx50.0
    Change-Id: I6327643a9cabbb8ec907f9953e1d2b05a43b8451

--- device.mk
+++ device.mk
@@ -27,6 +27,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
