0e55cdf00a18596640e68e7e5add3fa5a1db2cd9
LuK1337 <priv.luk@gmail.com>
2021-08-19 17:07:55 +0800

    umi: Build vndservicemanager
    * Needed for FCM >= R.
    
    Change-Id: Ibc27a98f5774a51caaab077c1d8e0a2a79bdffe5
    Signed-off-by: chaptsand <chaptsand@gmail.com>

--- device.mk
+++ device.mk
@@ -139,6 +139,10 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
+# Vendor service manager
+PRODUCT_PACKAGES += \
+    vndservicemanager
+
 # WLAN
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wlan/WCNSS_qcom_cfg_qca6390.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
