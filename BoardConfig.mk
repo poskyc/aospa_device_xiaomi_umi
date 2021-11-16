1f564c81f667aa93eaa025ac0b18343d51e5e34a
Mesquita <mesquita@aospa.co>
2021-11-16 17:11:00 +0800

    umi: Drop fwk detect props
     * Already defined in pa build
    
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -152,7 +152,6 @@ TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_COPY_OUT_VENDOR := vendor
 
 # Properties
-TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
